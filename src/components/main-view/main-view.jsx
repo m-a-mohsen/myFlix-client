8e35700728a33ec583ca4e5ef208c6d394fdd069
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-01-31 07:02:28 +0100

    Back to render.com deployment

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -46,8 +46,7 @@ export const MainView = () => {
   useEffect(() => {
     if (!token) return;
 
-    fetch('https://cfmovieapp2.azurewebsites.net/movies', {
-    // fetch('https://moviesapi2.onrender.com/movies', {
+    fetch('https://moviesapi2.onrender.com/movies', {
       headers: { Authorization: `Bearer ${token}` },
     })
       .then((response) => response.json())
